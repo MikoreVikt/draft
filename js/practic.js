275363fa83480933fd60cbbb22d2e734f637aa3c
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-09-23 01:09:54 +0300

    Update practic.js

--- js/practic.js
+++ js/practic.js
@@ -4,51 +4,51 @@
 // 3 зняти з карти (назву вводимо в prompt() та суму в prompt())
 // 4 додати на карту (назву вводимо в prompt() та суму в prompt())
 
-const bankSystem = {
-  privat: 1000,
-  mono: 1500,
+// const bankSystem = {
+//   privat: 1000,
+//   mono: 1500,
 
-  getSumm() {
-    const card = prompt();
-    if (this.hasOwnProperty(card)) {
-      return this[card];
-    }
-    return `Карты с таким названием нет`;
-  },
+//   getSumm() {
+//     const card = prompt();
+//     if (this.hasOwnProperty(card)) {
+//       return this[card];
+//     }
+//     return `Карты с таким названием нет`;
+//   },
 
-  addCard() {
-    const newCard = prompt();
-    if (!bankSystem.hasOwnProperty(newCard)) {
-      this[newCard] = 0;
-      return `Карта успешно создана`;
-    }
-    return `Такая карта уже существует`;
-  },
+//   addCard() {
+//     const newCard = prompt();
+//     if (!bankSystem.hasOwnProperty(newCard)) {
+//       this[newCard] = 0;
+//       return `Карта успешно создана`;
+//     }
+//     return `Такая карта уже существует`;
+//   },
 
-  withdraw() {
-    const nameCard = prompt(`Enter card name`);
+//   withdraw() {
+//     const nameCard = prompt(`Enter card name`);
 
-    if (this.hasOwnProperty(nameCard)) {
-      const summ = prompt(`Enter your summ`);
-      if (this[nameCard] >= summ) {
-        return (this[nameCard] -= summ);
-      } else {
-        return `Суммы на карте не достаточно`;
-      }
-    } else {
-      return `Такой карты нет`;
-    }
-  },
+//     if (this.hasOwnProperty(nameCard)) {
+//       const summ = prompt(`Enter your summ`);
+//       if (this[nameCard] >= summ) {
+//         return (this[nameCard] -= summ);
+//       } else {
+//         return `Суммы на карте не достаточно`;
+//       }
+//     } else {
+//       return `Такой карты нет`;
+//     }
+//   },
 
-  replenishment() {
-    const nameCard = prompt(`Enter card name`);
-    if (this.hasOwnProperty(nameCard)) {
-      const summ = prompt(`Enter your summ`);
-      return (this[nameCard] += +summ);
-    }
-    return `Такой карты нет`;
-  },
-};
+//   replenishment() {
+//     const nameCard = prompt(`Enter card name`);
+//     if (this.hasOwnProperty(nameCard)) {
+//       const summ = prompt(`Enter your summ`);
+//       return (this[nameCard] += +summ);
+//     }
+//     return `Такой карты нет`;
+//   },
+// };
 
 // console.log(bankSystem.getSumm());
 // console.log(bankSystem.addCard());
@@ -64,22 +64,22 @@ const bankSystem = {
 //2 параметр це масив з чоловічими іменами. (mens)
 // Функція повина повертати масив з жіночими іменами.
 
-const users = ['Artem', 'Anna', 'Larisa', 'Maksim', 'Svetlana', 'David', 'Roman', 'Olga'];
-const mens = ['Artem', 'Maksim', 'David', 'Roman'];
+// const users = ['Artem', 'Anna', 'Larisa', 'Maksim', 'Svetlana', 'David', 'Roman', 'Olga'];
+// const mens = ['Artem', 'Maksim', 'David', 'Roman'];
 
-function sortUsres(users, mens) {
-  const women = [];
-  console.log(users);
+// function sortUsres(users, mens) {
+//   const women = [];
+//   console.log(users);
 
-  for (const user of users) {
-    if (!mens.includes(user)) {
-      //   console.log(user);
-      women.push(user);
-    }
-  }
-  //   console.log(womans);
-  return women;
-}
+//   for (const user of users) {
+//     if (!mens.includes(user)) {
+//       //   console.log(user);
+//       women.push(user);
+//     }
+//   }
+//   //   console.log(womans);
+//   return women;
+// }
 
 // console.log(sortUsres(users, mens));
 
@@ -92,7 +92,7 @@ function sortUsres(users, mens) {
 // =========================================================================================
 // -----------------------------------------TASK 7------------------------------------------//
 // Потрібно мутувати масив так щоб залишились тільки (імена) елементи типу String.
-// const names = ['Anna', 3, 'Roma', 17, 'Kate', 14, 'Oleg'];
+// const arr = ['Anna', 3, 'Roma', 17, 'Kate', 14, 'Oleg'];
 
 // =========================================================================================
 // -----------------------------------------TASK 8------------------------------------------//
@@ -101,6 +101,24 @@ function sortUsres(users, mens) {
 // В результаті отримує меседж з дією яку повинен виконати
 // Світлофор може мати 4 стани green (GO), yellow (READY),red (STOP), будь що інше означає що сфітлофор не працює (BE CAREFUL)
 
+// const color = prompt();
+// switch (color) {
+//   case 'green':
+//     console.log('GO');
+//     break;
+
+//   case 'yellow':
+//     console.log('READY');
+//     break;
+
+//   case 'red':
+//     console.log('STOP');
+//     break;
+
+//   default:
+//     console.log('BE CAREFUL');
+// }
+
 // ==========================================================================================
 // -----------------------------------------TASK 9------------------------------------------//
 //Створіть функцію, яка приймає 1 параметр
@@ -108,20 +126,23 @@ function sortUsres(users, mens) {
 //Функція повертає меседж, як вказано у прикладах:
 //Використовувати switch()
 
-// function stringBuilder(users){}
+// function stringBuilder(users) {}
 
-// stringBuilder([])//"no one likes this";
-// stringBuilder(["Peter"])//"Peter likes this";
-// stringBuilder(["Jacob", "Alex"])//"Jacob and Alex like this";
-// stringBuilder(["Max", "John", "Mark"])//"Max, John and Mark like this";
-// stringBuilder(["Alex", "Jacob", "Mark", "Max"])//"Alex, Jacob and 2 others like this;
+// console.log(stringBuilder([])); //"no one likes this";
+// console.log(stringBuilder(['Peter'])); //"Peter likes this";
+// console.log(stringBuilder(['Jacob', 'Alex'])); //"Jacob and Alex like this";
+// console.log(stringBuilder(['Max', 'John', 'Mark'])); //"Max, John and Mark like this";
+// console.log(stringBuilder(['Alex', 'Jacob', 'Mark', 'Max'])); //"Alex, Jacob and 2 others like this";
 
 // =========================================================================================
 // -----------------------------------------TASK 10-----------------------------------------//
 // Cтворити функцію яка буде приймати 1 параметр та знаходити в масиві елементи що дублюються і потім ці елменти добавляти в новий масив
-// 1 масив чисел (arr)
+// 1 масив чисел (arr) //[1, 2, 3, 2, 1, 17, 19]
 
-// const numbers = [1, 2, 3, 2, 1, 17, 19]
+// function getCommonElements(array) {
+//   const uniqueNumbers = array.filter((el, idx, arr) => arr.indexOf(el) === idx);
 
-// function getCommonElements(arr){};
-// getCommonElements(numbers)
+//   return uniqueNumbers;
+// }
+
+// console.log(getCommonElements([1, 2, 3, 2, 1, 17, 19]));
