16e1ded629516de5ef41e7d2fb78f81d0f03475a
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-10-19 20:40:10 +0300

    Update index.js

--- node-tyt/index.js
+++ node-tyt/index.js
@@ -1,3 +1,18 @@
-// index.js
-const message = 'NodeJS in amazing!';
-console.log(message);
+// const joi = require(`joi`);
+// const shortid = require(`shortid`);
+
+// // const passwordSchema = joi.string().min(3).max(10).alphanum();
+// // console.log(passwordSchema.validate(`qqwed`));
+
+// console.log(shortid.generate());
+
+// =========================== Server ===========================
+// const express = require(`express`);
+// const app = express();
+// app.use(`*`, (req, res) => {
+//   console.log('Был запрос от браузера');
+//   res.send(`<h1>В ответ тебе html тег</h1>`);
+// });
+// const listener = app.listen(4444, () => {
+//   console.log(`Веб-сервер запущен на порте: ${listener.address().port}`);
+// });
